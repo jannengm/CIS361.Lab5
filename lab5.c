d177e9cd9fcbe2c840f6f626ee7e205eeb9d4224
Mark Jannenga <jannengm@mail.gvsu.edu>
2016-10-13 17:05:20 -0400

    Working to specifications, would like to clean up output.

--- lab5.c
+++ lab5.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define BUF_LEN 8
 
 struct Mark {
 	int x;
@@ -38,6 +41,7 @@ int main()
 	}
 
 	// use qsort() to sort data in list
+	qsort(list, size, sizeof(Mark), compare);
 
 	for (i = 0; i < size; i++)
 	{
@@ -45,6 +49,7 @@ int main()
 		if ( ++col % 5 == 0 )
 			printf("\n");
 	}
+	printf("\n");
 
 	fclose (fin);
 
@@ -55,24 +60,35 @@ int main()
 
 void getInfo (FILE * f, Mark * p)
 {
-	// read two integers from the input file and 
+	// read two integers from the input file and
 	// store them in the structure that p points to
+	char buffer[BUF_LEN];
 
+	if( fgets(buffer, BUF_LEN, f) == NULL ){
+		perror("Failed to read from input");
+		fclose(f);
+		exit(1);
+	}
+
+	p->x = atoi(strtok(buffer, " "));
+	p->y = atoi(strtok(NULL, " "));
 }
 
 void printInfo (FILE * f, Mark item)
 {
 	// display each mark in format of (x, y)
 	// and five marks per line
-
+		fprintf( f, "(%d, %d) ", item.x, item.y );
 }
 
 int compare (const void * a, const void * b)
 {
 	// compare two structures
-	// return 0 if they are equal, a value less than 0 if the first 
-	// comes before the second, a value greater than 0 otherwise 
-	
+	// return 0 if they are equal, a value less than 0 if the first
+	// comes before the second, a value greater than 0 otherwise
+	// if( ((Mark *)a)->x < ((Mark *)b)->x )
+	// 	return -1;
+	int diff_x = ( ((Mark *)a)->x - ((Mark *)b)->x ) * 100;
+	int diff_y = ( ((Mark *)a)->y - ((Mark *)b)->y );
+	return diff_x + diff_y;
 }
-
-
